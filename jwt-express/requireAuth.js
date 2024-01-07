0aabcf128f8b7a4fadde762e1e40fdadf4ce4438
Felix Mburu <153299113+felixmburudev@users.noreply.github.com>
2024-01-07 10:17:49 +0300

    Update requireAuth.js

--- jwt-express/requireAuth.js
+++ jwt-express/requireAuth.js
@@ -24,21 +24,3 @@ module.exports = requireAuth
 
 
 
-// const jwt = require('jsonwebtoken');
-
-// const requireAuth = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//     return res.status(401).json({ message: 'Unauthorized' });
-//   }
-//   const token = authHeader.split(' ')[1];
-//   try {
-//     const decodedToken = jwt.verify(token, 'hgfytffvh');
-//     req.user = decodedToken; 
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-// h  
-// module.exports = requireAuth;
